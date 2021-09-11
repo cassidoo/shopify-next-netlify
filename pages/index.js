8d218a41a4bfa51683441f8c540f37302c8d34c7
cassidoo <cassidoo@users.noreply.github.com>
2021-09-10 23:01:32 -0500

    Remove unused import

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import ProductListing from '@components/ProductListing';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
-import { useAppContext } from '../state';
 
 export default function Home({ products }) {
   return (
